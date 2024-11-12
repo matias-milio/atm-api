8cbdebc0ed47ce5a726226d3908c10bcd7846c58
Matias Milio <gaudiomatias@live.com>
2024-11-12 18:17:18 -0300

    Project structure, base classes for result and db entities

---
+++ ATM.Core/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace ATM.Core.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ ATM.Core/Result.cs
@@ -0,0 +1,33 @@
+﻿namespace ATM.Core
+{
+    public class Result<TValue, TError>
+    {
+        public readonly TValue? Value;
+        public readonly TError? Error;
+        public bool IsSuccess;
+        private Result(TValue value)
+        {
+            IsSuccess = true;
+            Value = value;
+            Error = default;
+        }
+
+        private Result(TError error)
+        {
+            IsSuccess = false;
+            Value = default;
+            Error = error;
+        }
+
+        public static implicit operator Result<TValue, TError>(TValue value) => new(value);
+
+        public static implicit operator Result<TValue, TError>(TError error) => new(error);
+
+        public Result<TValue, TError> Match(Func<TValue, Result<TValue, TError>> success, Func<TError, Result<TValue, TError>> failure)
+        {
+            if (IsSuccess)            
+                return success(Value!);            
+            return failure(Error!);
+        }
+    }
+}

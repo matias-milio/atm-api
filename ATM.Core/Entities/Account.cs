a868d261afa4fb79ddc21a81ea3418225e032c4a
Matias Milio <gaudiomatias@live.com>
2024-11-12 18:56:44 -0300

    Entities correction, db context creation

--- ATM.Core/Entities/Account.cs
+++ ATM.Core/Entities/Account.cs
@@ -3,7 +3,8 @@
     public class Account : BaseEntity
     {
         public required string Number { get; set; }
-        public decimal Balance { get; set; }
+        public required decimal Balance { get; set; }        
+        public required int CardId { get; set; }
         public virtual required Card Card { get; set; }
     }   
 }
---
+++ ATM.Infrastructure/AtmDbContext.cs
@@ -0,0 +1,32 @@
+﻿using ATM.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ATM.Infrastructure
+{
+    public class AtmDbContext(DbContextOptions<AtmDbContext> options) : DbContext(options)
+    {
+        public DbSet<Card>Cards{ get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<CardHolder> CardHolders { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Card>()
+                .HasOne(c => c.CardHolder)
+                .WithMany(ch => ch.Cards)
+                .HasForeignKey(c => c.CardHolderId);
+
+            modelBuilder.Entity<Card>()
+                .HasOne(c => c.Account)
+                .WithOne(a => a.Card)
+                .HasForeignKey<Account>(a => a.CardId);
+
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.Card)
+                .WithMany(c => c.Transactions)
+                .HasForeignKey(t => t.CardId);
+        }
+    }
+}
